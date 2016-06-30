4c21c112211d1af4e8e48e458803cf487a60fe31
Moakeson <moakeson25@gmail.com>
2016-06-30 09:34:59 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+function submit(){
+    var item = document.getElementById("list").value;
+    var list = document.getElementById("list2");
+    var listitem = document.createElement("li");
+    listitem.appendChild(document.createTextNode(item));
+    var button = document.createElement("input");
+    button.type="button";
+    
+    function buttonHasBeenClicked(event){
+        var parent = event.srcElement.parentElement.parentElement;
+        parent.removeChild(event.srcElement.parentElement);
+    }
+    button.onclick=buttonHasBeenClicked;
+    
+    listitem.appendChild(button);
+    list.appendChild(listitem);
+    document.getElementById("placeholder").value = '';
+}
